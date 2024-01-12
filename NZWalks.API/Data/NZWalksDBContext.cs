d3a7a30454d07fbebe7973013bd408c4b26de221
HarshadKulkarni <harshadkulkarni7578@gmail.com>
2024-01-13 00:11:32 +0530

    Added Domain Models and DB conetxt class.

---
+++ NZWalks.API/Data/NZWalksDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.DomainModels;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDBContext:DbContext
+    {
+        public NZWalksDBContext(DbContextOptions dbContextOptions):base(dbContextOptions)
+        {
+                
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<Difficulty> Difficulties { get; set; }
+    }
+}
---
+++ NZWalks.API/Models/DomainModels/Difficulty.cs
@@ -0,0 +1,8 @@
+﻿namespace NZWalks.API.Models.DomainModels
+{
+    public class Difficulty
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
