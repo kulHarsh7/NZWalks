27be03bab3f5ac03193e9cd5b310ab875d50a8f8
HarshadKulkarni <harshadkulkarni7578@gmail.com>
2024-03-10 18:45:26 +0530

    Add identity db context

---
+++ NZWalks.API/Data/NZWalksAuthDBContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksAuthDBContext : IdentityDbContext
+    {
+        public NZWalksAuthDBContext(DbContextOptions<NZWalksAuthDBContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerId = "8add3282-a052-4c5a-9342-6c6d974da376";
+            var writerId = "45298d55-d636-41a2-92bc-f042148eea83";
+            var roles = new List<IdentityRole>()
+            {
+              new IdentityRole()
+              {
+                  Id=readerId,
+                  Name="Reader",
+                  NormalizedName="Reader".ToUpper(),
+                  ConcurrencyStamp=readerId
+              },
+                new IdentityRole()
+              {
+                  Id=writerId,
+                  Name="Writer",
+                  NormalizedName="Writer".ToUpper(),
+                  ConcurrencyStamp=writerId
+              }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
+       
+    }
+}
--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -1,12 +1,15 @@
 
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using NZWalks.API.Data;
 using NZWalks.API.Mapping;
 using NZWalks.API.Middleware;
 using NZWalks.API.Repositories.Implementation;
 using NZWalks.API.Repositories.Interfaces;
 using Serilog;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,14 +27,29 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<NZWalksDBContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("NZwalksConnectionString")));
+
+builder.Services.AddDbContext<NZWalksAuthDBContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("AuthConnectionString")));
 
 builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
 builder.Services.AddScoped<IRegionRepository, RegionRepository>();
 builder.Services.AddScoped<IDifficultyRepository, DifficultyRepository>();
 builder.Services.AddScoped<IWalkRepository, WalkRepository>();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).
+    AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidateLifetime = true,
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    });
 
 var app = builder.Build();
 
@@ -46,6 +64,7 @@ app.UseMiddleware<ExceptionHandler>();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
